93d1f109d8a07dc56372b94baba2d71f295d0395
EvanLube <ebl1002@sru.edu>
2025-02-05 18:30:56 -0500

    Update signup.h

--- signup.h
+++ signup.h
@@ -1,14 +1,17 @@
 #pragma once
 #include <drogon/HttpController.h>
+#include <drogon/orm/DbClient.h>
 
 class SignupController : public drogon::HttpController<SignupController>
 {
 public:
     METHOD_LIST_BEGIN
-    // Add SignupController:: to specify which class the signup method belongs to
     METHOD_ADD(SignupController::signup, "/signup", drogon::Post);
     METHOD_LIST_END
     
     void signup(const drogon::HttpRequestPtr& req,
                std::function<void(const drogon::HttpResponsePtr&)>&& callback);
+private:
+    // Database client pointer
+    std::shared_ptr<drogon::orm::DbClient> dbClient;
 };
